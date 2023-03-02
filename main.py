350ddb8f192a8d3a69fbe0a3d9b33937cdc94d45
A S Md Ferdousul Haque <ferdousul.haque@gmail.com>
2023-03-02 08:30:17 +0600

    Adding fastAPI and file upload

--- main.py
+++ main.py
@@ -2,20 +2,44 @@
 # Import the required Module
 import tabula
 import xlwt
+from fastapi import FastAPI, File, UploadFile
+from starlette.responses import HTMLResponse
+
+app = FastAPI()
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/pdf-converter/csv" enctype="multipart/form-data" method="post">
+<input name="files" type="file" accept="application/pdf">
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
+
+@app.post("/pdf-converter/{type}")
+def read_root(type: str, pdf_file: UploadFile):
+    pdf_file.write(pdf_file.file.read())
+
+    # contents = pdf_file.file.read()
+    df = tabula.read_pdf("input/white.pdf", pages='all')[0]
+
+    return {"filename": file.filename}
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.the
     # Read a PDF File
-    df = tabula.read_pdf("white.pdf", pages='all')[0]
+    df = tabula.read_pdf("input/white.pdf", pages='all')[0]
     # convert PDF into CSV
     #tabula.convert_into("white.pdf", "white.csv", output_format="csv", pages='all')
     #df.head()
-    df.to_excel("white.xls")
+    df.to_excel("output/white.xls")
     #print(df)
     #f = open("demofile2.xls", "a")
     #f.write(df)
@@ -23,7 +47,5 @@ def print_hi(name):
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
---
+++ readme.md
@@ -0,0 +1,13 @@
+# py-pdf-converter
+A simple python pdf converter. For one of my job, I needed to convert pdf to excel, csv. However, not able to find any package for PHP that would serve the purpose. So this is a standalone script that will do the conversion for me.
+
+### Packages
+I have used the following packages of python to build
+
+- tabula
+- xlwt
+- panda
+- fastapi
+
+### Suggestions
+Please do mail me for any suggestions, I will be glad to include [ferdousul.haque@gmail.com](ferdousul.haque@gmail.com)
